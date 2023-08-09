f4106579a995081586f114b3b1839508fb7fe4fd
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-08-09 09:58:21 +0530

    Hookstask1completed

--- vite-project/src/components/Index.jsx
+++ vite-project/src/components/Index.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Index() {
-    let name = "this is me Saloni Gupta, Full Stack Web Developer";   
+const [Data, setData] = useState("This is me Saloni Gupta")
+  // let name = "this is me Saloni Gupta, Full Stack Web Developer";
   const HandleClick = () => {
-     name = "SALONI GUPTA"
-     console.log(name)
+    setData("Saloni Gupta Wb developer")
+ let val = Data; 
+    if (val === "This is me Saloni Gupta") {
+      setData("SALONI GUPTA")
+    }
+    else {
+       setData("This is me Saloni Gupta")
+    }
   }
-    return (
+  return (
 
     <div>
-        <p>{name}</p>
-        <button onClick={HandleClick}>Click me</button>
+      <p id="demo">{Data}</p>
+      <button onClick={HandleClick}>Click me</button>
     </div>
   )
 }
